b4d76d23df0beb64864728e9ad25409f36c41647
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-05-07 23:05:25 +0530

    landing page ui 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
-import Image from "next/image";
+import Navbar from "@/components/global/navbar";
+import { ContainerScroll } from "@/components/global/container-scroll-animation";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Navbar />
+      <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
+        <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
+        <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
+          <ContainerScroll
+            titleComponent={
+              <div className="flex items-center flex-col">
+                <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+                  Make Your Own AI With DevAI
+                </h1>
+              </div>
+            }
+          />
+        </div>
+      </section>
+    </main>
+  );
 }
