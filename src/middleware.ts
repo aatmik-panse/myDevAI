0f9a43de1e164dc47bbc270ce37282d9c15f6aa4
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-09-01 12:22:37 +0530

    dependencies refactored

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,27 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-const isPublicRoute = createRouteMatcher([
-  "/agency/sign-in(.)",
-  "/agency/sign-up(.)",
-  "/site",
-  "/api/uploadthing",
-]);
-
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    return auth().redirectToSignIn({ returnBackUrl: request.url });
-  }
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/api/clerk-webhook",
+    "/api/drive-activity/notification",
+    "/api/payment/success",
+  ],
+  ignoredRoutes: [
+    "/api/auth/callback/discord",
+    "/api/auth/callback/notion",
+    "/api/auth/callback/slack",
+    "/api/flow",
+    "/api/cron/wait",
+  ],
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?].(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+// https://www.googleapis.com/auth/userinfo.email
+// https://www.googleapis.com/auth/userinfo.profile
+// https://www.googleapis.com/auth/drive.activity.readonly
+// https://www.googleapis.com/auth/drive.metadata
+// https://www.googleapis.com/auth/drive.readonly
